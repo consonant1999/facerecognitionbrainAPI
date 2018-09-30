baafd03b808ac1fd29eb2469e19598d6040a441f
consonant <consonant1999@gmail.com>
2018-09-30 16:00:07 +0800

    Change database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image')
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-horizontal-31759',
 		user: 'consonant',
 		password: '',
 		database: 'smart-brain'
